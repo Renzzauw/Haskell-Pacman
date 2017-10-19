475ac4e539154aea5aa1de3434eb1a613f5b2b9f
Steven <stevenheinen01@gmail.com>
2017-10-19 16:40:42 +0200

    kleine letters

--- src/Level.hs
+++ src/Level.hs
@@ -8,11 +8,11 @@ data Row = [Field]
 data Level = [Row]
 
 -- Function for converting a Char from the textfilefile to a Field
-TextToField :: Char -> Field 
-TextToField 'P' = Player
-TextToField 'W' = Wall
-TextToField '.' = Point
-TextToField '*' = BigPoint
-TextToField 'E' = Enemy
-TextToField ' ' = Empty
+textToField :: Char -> Field 
+textToField 'P' = Player
+textToField 'W' = Wall
+textToField '.' = Point
+textToField '*' = BigPoint
+textToField 'E' = Enemy
+textToField ' ' = Empty
 
