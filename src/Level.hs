84e0f12d588f44aed6ca1137da97803f32ee3e98
Steven Heinen <stevenheinen01@gmail.com>
2017-10-22 12:23:00 +0200

    IO Level kan nu gemaakt worden door een .txt-file te lezen

--- src/Level.hs
+++ src/Level.hs
@@ -4,10 +4,9 @@ module Level where
 data Field = Player | Wall | Point | BigPoint | Enemy | Empty
 
 type Row = [Field]
-
 type Level = [Row]
 
--- Function for converting a Char from the textfilefile to a Field
+-- Function for converting a Char from the textfile to a Field
 textToField :: Char -> Field 
 textToField 'P' = Player
 textToField 'W' = Wall
@@ -16,8 +15,8 @@ textToField '*' = BigPoint
 textToField 'E' = Enemy
 textToField ' ' = Empty
 
-{-loadLevel :: FilePath -> Level
+loadLevel :: FilePath -> IO Level
 loadLevel filePath = do
     text <- readFile filePath
     let rows = lines text
-    (map . map) textToField rows-}
+    return $ (map . map) textToField rows
--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Controller
 import Model
 import View
+import Level
 
 import Graphics.Gloss.Interface.IO.Game
 
