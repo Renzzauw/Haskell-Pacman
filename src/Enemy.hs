c3d3a0093fceda5a705fbbe9f670b2661beab374
Renzo Schindeler <renzo@schindeler.nl>
2017-11-02 16:33:24 +0100

    Nog meer Enemy aanpassingen en comments enzo

--- src/Enemy.hs
+++ src/Enemy.hs
@@ -3,19 +3,19 @@ module Enemy where
 import Level
 import Model
 
+-- Function that returns a Direction for the enemy to move into based on the player position
 lookForPlayer :: GameState -> Position -> Position -> Direction
-lookForPlayer gs playerpos@(px, py) enemypos@(ex, ey) | py < ey && surroundings !! 0 == WallField  = DirNone
-                                                      | py < ey && surroundings !! 0 /= WallField  = DirUp
-                                                      | py > ey && surroundings !! 1 == WallField  = DirNone
-                                                      | py > ey && surroundings !! 1 /= WallField  = DirDown
-                                                      | px < ex && surroundings !! 2 == WallField  = DirNone
-                                                      | px < ex && surroundings !! 2 /= WallField  = DirLeft
-                                                      | px > ex && surroundings !! 3 == WallField  = DirNone
-                                                      | px > ex && surroundings !! 3 /= WallField  = DirRight
-                                                      | otherwise                                  = DirNone
+lookForPlayer gs playerpos@(px, py) enemypos@(ex, ey) | py < ey && surroundings !! 0 == WallField = DirNone
+                                                      | py < ey && surroundings !! 0 /= WallField = DirUp
+                                                      | py > ey && surroundings !! 1 == WallField = DirNone
+                                                      | py > ey && surroundings !! 1 /= WallField = DirDown
+                                                      | px < ex && surroundings !! 2 == WallField = DirNone
+                                                      | px < ex && surroundings !! 2 /= WallField = DirLeft
+                                                      | px > ex && surroundings !! 3 == WallField = DirNone
+                                                      | px > ex && surroundings !! 3 /= WallField = DirRight
+                                                      | otherwise                                 = DirNone
                                                     where surroundings = getSurroundingFields gs enemypos
                                                    
-
 -- Function that returns a list of Points that surround a given Point
 getSurroundingFields :: GameState -> Position -> [FieldType]
 getSurroundingFields gs (x, y) = map fst (filter (isValidMoveField gs) fieldList)
@@ -24,15 +24,12 @@ getSurroundingFields gs (x, y) = map fst (filter (isValidMoveField gs) fieldList
                                               right = ((getFieldType gs ((x + 1), y)), ((x + 1), y))
                                               down  = ((getFieldType gs (x, (y + 1))), (x, (y + 1)))
                                               fieldList   = [up, down, left, right]
- 
+
+-- Function that returns the FieldType of a field at a given position                                              
 getFieldType :: GameState -> Position -> FieldType
 getFieldType gs (x, y) = fst (((level gs) !! round y) !! round x)                              
     
-isPlayerDead :: Position -> Position -> Bool 
-isPlayerDead player enemy   | player == enemy = True
-                            | otherwise       = False
-    
-    -- Function that checks if a Field is valid to move to for the enemy
+-- Function that checks if a Field is valid to move to for the enemy
 isValidMoveField :: GameState -> Field -> Bool
 isValidMoveField gs (fieldType, (x, y)) | fieldType == WallField                          = False -- Check if field is a wall
                                         | x > 0 && x < maxWidth && y > 0 && y < maxHeight = True  -- doublecheck for movement outside of bounds
