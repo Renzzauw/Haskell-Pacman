f231ef51bf9713acad7d24839754a2efb3953d53
Steven Heinen <stevenheinen01@gmail.com>
2017-11-10 13:05:30 +0100

    Meer comments

--- src/Model.hs
+++ src/Model.hs
@@ -48,24 +48,29 @@ data GameState =
       | HelpScreen
       | ControlsScreen
 
+-- Data type for a powerup
 data PowerUp = PowerUp { 
     puType :: PowerUpType, 
     duration :: Float, 
     position :: Position }
         deriving (Eq)
     
+-- The different types of powerups in the game
 data PowerUpType = SpeedUp | EatEnemies | InvertedEnemies | NoPowerUp
     deriving (Eq, Enum)
 
+-- Data type for an animation
 data Animation = Animation {
     animationType :: AnimationType,
     animPos :: Position,
     startTime :: Float,
     stopTime :: Float }
 
+-- The different types of animations in the game
 data AnimationType = RedEnemyDied | BlueEnemyDied
     deriving (Eq)
 
+-- Function that creates the initial of a level based on its filepath
 initialState :: FilePath -> IO GameState
 initialState filePath = do
     (_level, _points, _player, _player2, _enemies) <- loadLevel ("Levels/" ++ filePath)
@@ -74,12 +79,14 @@ initialState filePath = do
     return state
     where   _powerUp = PowerUp NoPowerUp 0 (0, 0)
 
+-- GameState for choosing the levels, IO because it has to get the directory-contents of folder Levels
 levelChooserState :: IO GameState
 levelChooserState = do
     _levels <- getLevels
     let state = LevelChooser _levels
     return state
 
+-- Constances for the player- and enemyvelocities
 playerVelocity :: Float
 playerVelocity = 0.04
 
