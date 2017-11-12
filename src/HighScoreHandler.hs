d27dcdc38f97aad2d943d29bc4f3acfefaec3404
Renzzauw <renzo@schindeler.nl>
2017-11-12 12:27:46 +0100

    nog een paar comments + aanpassing spawns Pvp level

--- src/HighScoreHandler.hs
+++ src/HighScoreHandler.hs
@@ -23,28 +23,28 @@ getScore string = words string !! 1
 -- Function that returns a list of all the scores using the function above
 getScores :: String -> [String]
 getScores string = map getScore usedRows
-    where   rows = lines string
+    where   rows     = lines string
             usedRows = init (tail rows)
 
 -- Newscore takes the new score and the existing scores as a string and inserts the new score in the old scores. It returns the first 5 scores
 newScore :: Int -> String -> [Int]
 newScore score string = take 5 (reverse insertedNewScore)
-    where   list = getScores string
-            ints = map read list :: [Int]
-            sortedInts = sort ints
+    where   list             = getScores string
+            ints             = map read list :: [Int]
+            sortedInts       = sort ints
             insertedNewScore = insert score sortedInts
 
 -- Function that puts the new scores into the right format
 writeHighScore :: Int -> [Int] -> String
 writeHighScore score highScores = total
     where   header = "Highscores: "
-            s1 = "1. " ++ show (head highScores)
-            s2 = "2. " ++ show (highScores !! 1)
-            s3 = "3. " ++ show (highScores !! 2)
-            s4 = "4. " ++ show (highScores !! 3)
-            s5 = "5. " ++ show (highScores !! 4)
-            s6 = "Last score: " ++ show score
-            total = unlines (header : s1 : s2 : s3 : s4 : s5 : [s6])
+            s1     = "1. " ++ show (head highScores)
+            s2     = "2. " ++ show (highScores !! 1)
+            s3     = "3. " ++ show (highScores !! 2)
+            s4     = "4. " ++ show (highScores !! 3)
+            s5     = "5. " ++ show (highScores !! 4)
+            s6     = "Last score: " ++ show score
+            total  = unlines (header : s1 : s2 : s3 : s4 : s5 : [s6])
 
 -- Checks if the Highscores.txt already exists, if it doesn't, create it and put the standard scores in it. After this, open the file in ReadMode
 createOrOpenHighScores :: IO Handle
