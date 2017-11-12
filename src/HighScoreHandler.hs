dc9e77184a94eb5e2298ca23d8ebf35a3cd9b3be
Steven Heinen <stevenheinen01@gmail.com>
2017-11-12 12:21:27 +0100

    Comments in the highscorehandler

--- src/HighScoreHandler.hs
+++ src/HighScoreHandler.hs
@@ -16,15 +16,17 @@ Highscores:
 Last score: 0
 -}
 
+-- Function that takes a line (for instance: "1. 0") from the input and takes the second part of it as a string (the score)
 getScore :: String -> String
-getScore string = whatToRead
-    where   whatToRead = words string !! 1
+getScore string = words string !! 1
 
+-- Function that returns a list of all the scores using the function above
 getScores :: String -> [String]
 getScores string = map getScore usedRows
     where   rows = lines string
             usedRows = init (tail rows)
 
+-- Newscore takes the new score and the existing scores as a string and inserts the new score in the old scores. It returns the first 5 scores
 newScore :: Int -> String -> [Int]
 newScore score string = take 5 (reverse insertedNewScore)
     where   list = getScores string
@@ -32,6 +34,7 @@ newScore score string = take 5 (reverse insertedNewScore)
             sortedInts = sort ints
             insertedNewScore = insert score sortedInts
 
+-- Function that puts the new scores into the right format
 writeHighScore :: Int -> [Int] -> String
 writeHighScore score highScores = total
     where   header = "Highscores: "
@@ -43,6 +46,7 @@ writeHighScore score highScores = total
             s6 = "Last score: " ++ show score
             total = unlines (header : s1 : s2 : s3 : s4 : s5 : [s6])
 
+-- Checks if the Highscores.txt already exists, if it doesn't, create it and put the standard scores in it. After this, open the file in ReadMode
 createOrOpenHighScores :: IO Handle
 createOrOpenHighScores = do
     exists <- doesFileExist "Highscores.txt"
@@ -50,6 +54,7 @@ createOrOpenHighScores = do
     openFile "Highscores.txt" ReadMode
     where   startHighScores = "Highscores:\n1. 0\n2. 0\n3. 0\n4. 0\n5. 0\nLast score: 0"
 
+-- Update the highscores by using all the functions above, called from the Controller.
 updateHighScore :: Int -> IO ()
 updateHighScore score = do
     h2 <- createOrOpenHighScores
